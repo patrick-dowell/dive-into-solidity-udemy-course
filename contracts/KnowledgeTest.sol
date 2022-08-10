ce59deb72b7a52251f19a3a3bf555c9326ab5973
Patrick Dowell <patrick.dowell@gmail.com>
2022-08-09 18:23:23 -0700

    Adding KnowledgeTest and Lottery files with answers

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -6,8 +6,41 @@ contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
+    }
+
+    receive() external payable {
+
+    }
+
+    fallback() external payable {
+
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable destination) public returns (bool, bytes memory) {
+        require(msg.sender == owner, "ONLY_OWNER");
+
+        (bool success, bytes memory returnBytes) = destination.call{value: getBalance()}("");
+        return (success, returnBytes);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata s1, string calldata s2) public pure returns (string memory) {
+        return string(abi.encodePacked(s1, s2));
     }
 }
